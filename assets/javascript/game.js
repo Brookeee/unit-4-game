166895d8c5a7bddee9bc00f73a6b6669466e7969
Brookee <oliver.brooke.a@gmail.com>
2019-05-14 21:21:30 -0700

    initial pseudocode

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+// Create on click buttons for each crystal? *Random value between 1-12*
+// Random number variable/math.floor(math.random). *random number between 19-120*
+// document onkeyup for user clicking on crystal
+// Code to hide display of # value of the crystals 
+// Code to add crystal qty to equal random # 
+// If player wins, add to (wins) score and reset game
+// If player loses, add to losses score and reset game
+// New random number generated once game resets [cont line 9]
+//Crystals have a new value for player to equal to. User's score resets
+// Keep overall score **
+
